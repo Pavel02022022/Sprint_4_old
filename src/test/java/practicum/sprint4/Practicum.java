b48a70898e02da73e58f02284f39a152163edd33
Pavel Krivosheev <mp413413413@gmail.com>
2024-05-10 00:59:40 +0300

    11

--- src/test/java/practicum/sprint4/Practicum.java
+++ src/test/java/practicum/sprint4/Practicum.java
@@ -1,6 +1,7 @@
 package practicum.sprint4;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -52,8 +53,7 @@ public class Practicum {
         String bbb = driver.findElement(By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[1]/div[2]/p")).getText();
         System.out.println(bbb);
 
-        String bbbb = driver.findElement(By.xpath(".//div[contains(text(), 'Хочу сразу несколько самокатов! Так можно?')]/p")).getText();
-        System.out.println(bbbb);
+
 
     }
     @Test
@@ -72,9 +72,6 @@ public class Practicum {
 
         driver.findElement(l1).click();
         driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
-
-
-
     }
 
     @Test
@@ -107,17 +104,50 @@ public class Practicum {
             System.out.println("The element does not have the hidden attribute.");
         }
 
-        /*
-        driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
-        String hidden = driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]")).getAttribute("hidden");
-        String hidden2 = driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]")).getText();
-        System.out.println(hidden);
+    }
+
+    @Test
+    public void texts(){
+        driver.get(path);
+
+        String t1 = "Сколько это стоит? И как оплатить?";
+        By l1 = By.xpath(".//div[contains(text(), '" + t1 + "')]");
+        String tt1 = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+
+        String t2 = "Хочу сразу несколько самокатов! Так можно?";
+        By l2 = By.xpath(".//div[contains(text(), '" + t2 + "')]");
+        String tt2 = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+
+
+        // Скролл до кнопки
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(l1));
+
+        // Привязывемся к id для дальнейшего поиска текста выпадающего списка
+        String id =  driver.findElement(l1).getAttribute("aria-controls");
+        //System.out.println(id);
 
         driver.findElement(l1).click();
-        driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
+
+        // Выпадающий список открылся
+        Assert.assertEquals("true", driver.findElement(l1).getAttribute("aria-expanded"));
+
+        // Текст больше не скрыт
+        Assert.assertEquals(null, driver.findElement(By.xpath(".//div[@id ='" + id + "']")).getAttribute("hidden"));
 
 
-         */
+        // Проверка, что открылся соответствующий текст
+        String text = new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(".//div[@id ='" + id + "']")))).getText();
+
+        System.out.println(text);
+        Assert.assertEquals(text, tt1);
+
+
+
+
+
+
+
 
 
     }
