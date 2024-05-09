f86c7b7be536bc3d27ac2f69021f8c936bf70dfa
Pavel Krivosheev <mp413413413@gmail.com>
2024-05-09 15:29:18 +0300

    11

--- src/test/java/ru/yandex/practicum/sprint4/MainPage.java
+++ src/test/java/ru/yandex/practicum/sprint4/MainPage.java
@@ -1,5 +1,23 @@
 package ru.yandex.practicum.sprint4;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class MainPage {
-    
+    private WebDriver driver;
+
+    //Кнопка "Заказать"
+    private By orderButton = By.className("Button_Button__ra12g");
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Нажать кнопку "Заказать"
+    public void pressOrederButton(){
+        driver.findElement(orderButton).click();
+    }
+
+
+
 }
