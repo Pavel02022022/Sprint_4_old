1be0d3af64fbe248bca604a9b9d11bed7e4e24a1
Pavel Krivosheev <mp413413413@gmail.com>
2024-05-09 22:30:03 +0300

    11

--- src/main/java/practicum/sprint4/pom/MainPage.java
+++ src/main/java/practicum/sprint4/pom/MainPage.java
@@ -1,22 +1,47 @@
 package practicum.sprint4.pom;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPage {
     private WebDriver driver;
 
-    //Кнопка "Заказать"
+    // Кнопка "Заказать"
     private By orderButton = By.className("Button_Button__ra12g");
+    // "Вопрос о важном"
+    //private By accordionButton = By.className("accordion__button");
+    private By accordionButton = By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[1]/div[1]/div");
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
     //Нажать кнопку "Заказать"
-    public MainPage pressOrederButton(){
+    public MainPage pressOrderButton(){
         driver.findElement(orderButton).click();
         return this;
+    }
+
+    // Нажать на один из "Вопросов о важном"
+    public MainPage pressAccordionButton(){
+        // Скролл до кнопки
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(accordionButton));
+        // Нажатие
+        driver.findElement(accordionButton).click();
+        return this;
+    }
+
+    public void dsfs(){
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(accordionButton));
+        int abuttons = driver.findElements(accordionButton).size();
+        System.out.println(abuttons);
     }
 
 
--- src/test/java/practicum/sprint4/Practicum.java
+++ src/test/java/practicum/sprint4/Practicum.java
@@ -3,14 +3,17 @@ package practicum.sprint4;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import practicum.sprint4.pom.MainPage;
 
 import java.time.Duration;
-
-import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class Practicum {
     private WebDriver driver;
@@ -20,7 +23,7 @@ public class Practicum {
     public void setup() {
         // Настройки Хрома
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
+        //options.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
         // Драйвер для браузера Chrome
         driver = new ChromeDriver(options);
         // Ожидание
@@ -28,13 +31,85 @@ public class Practicum {
     }
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
         // Переход на страницу тестового приложения
         driver.get(path);
 
         MainPage mainPage = new MainPage(driver);
 
-        mainPage.pressOrederButton();
+        mainPage.dsfs();
+        mainPage.pressAccordionButton();
+
+        String aa = driver.findElement(By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[1]/div[1]/div")).getText();
+        System.out.println(aa);
+
+        driver.findElement(By.xpath(".//div[contains(text(), 'Сколько это стоит? И как оплатить?')]")).click();
+        String bb = driver.findElement(By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[1]/div[2]/p")).getText();
+        System.out.println(bb);
+        driver.findElement(By.xpath(".//div[contains(text(), 'Хочу сразу несколько самокатов! Так можно?')]")).click();
+
+        // /html/body/div/div/div/div[5]/div[2]/div/div[1]/div[1]/div
+        String bbb = driver.findElement(By.xpath("/html/body/div/div/div/div[5]/div[2]/div/div[1]/div[2]/p")).getText();
+        System.out.println(bbb);
+
+        String bbbb = driver.findElement(By.xpath(".//div[contains(text(), 'Хочу сразу несколько самокатов! Так можно?')]/p")).getText();
+        System.out.println(bbbb);
+
+    }
+    @Test
+    public void asd(){
+        driver.get(path);
+
+        String t1 = "Сколько это стоит? И как оплатить?";
+        By l1 = By.xpath(".//div[contains(text(), '" + t1 + "')]");
+
+        // Скролл до кнопки
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(l1));
+        driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
+        String hidden = driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]")).getAttribute("hidden");
+        String hidden2 = driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]")).getText();
+        System.out.println(hidden);
+
+        driver.findElement(l1).click();
+        driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
+
+
+
+    }
+
+    @Test
+    public void hidden(){
+        driver.get(path);
+
+        String t1 = "Сколько это стоит? И как оплатить?";
+        By l1 = By.xpath(".//div[contains(text(), '" + t1 + "')]");
+        By l11 = By.className("accordion__panel");
+
+        // Скролл до кнопки
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(l1));
+        //driver.findElement(l1).click();
+
+        WebElement accordionPanel = driver.findElement(l11);
+        String hiddenAttribute = accordionPanel.getAttribute("hidden");
+        System.out.println(hiddenAttribute);
+        if (hiddenAttribute != null && hiddenAttribute.equals("true")) {
+            System.out.println("The element has the hidden attribute.");
+        } else {
+            System.out.println("The element does not have the hidden attribute.");
+        }
+
+        /*
+        driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
+        String hidden = driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]")).getAttribute("hidden");
+        String hidden2 = driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]")).getText();
+        System.out.println(hidden);
+
+        driver.findElement(l1).click();
+        driver.findElement(By.xpath(".//p[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
+
+
+         */
+
 
     }
 
